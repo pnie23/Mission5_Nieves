02633065312fe7eed8c447061c88527810af937c
pnie23 <pdnie567@gmail.com>
2024-02-08 09:40:20 -0700

    Add project files.

---
+++ Mission5_Nieves/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿$('#btnCalculate').click(function () {
+	// Show loading animation
+	$('#loading').show();
+
+	// Select input fields
+	var hours = $('#coachHours').val();
+	var hourlyPrice = $('#coachCost').val();
+
+	// Calculate total cost
+	var totalCost = hours * hourlyPrice;
+
+	// Update total cost output field
+	totalCost = (totalCost.toFixed(2)); // Format to 2 decimal places
+	$("#costEstimate").text('$' + totalCost);
+});
